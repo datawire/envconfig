82bdaecc500350edf066f5b049c08c6544d386f0
Luke Shumaker <lukeshu@datawire.io>
2021-12-15 21:29:03 -0700

    Set up as a separate Go module

--- envconfig_test.go
+++ envconfig_test.go
@@ -11,7 +11,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
-	"github.com/datawire/apro/v2/cmd/amb-sidecar/types/internal/envconfig"
+	"github.com/datawire/envconfig"
 )
 
 func TestAbsoluteURL(t *testing.T) {
